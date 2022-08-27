a4633de0f0eb12fe8758590634dd2d438987f47d
Panuvit Suckchuai <panuvit1165@gmail.com>
2022-08-27 13:53:57 +0700

    Part 4: Connect

--- components/Weather.js
+++ components/Weather.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { ImageBackground, Text, StyleSheet, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ImageBackground, Text, StyleSheet, View } from 'react-native';
 import Forecast from './Forecast';
 import Constants from 'expo-constants';
 
@@ -15,37 +15,46 @@ export default function Weather(props) {
 
     });
 
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=414dcf790bc29c3207a05f7c93f02d6d`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+
+        }
+
+    }, [props.zipCode])
+
     return (
-        
-
-        
-            <ImageBackground source={require("../bg1.jpg")} style = {styles.backdrop}>
-
-                <View style = {styles.highlight}>
 
 
-                    <Text style = {styles.textStyle}>Zip Code is {props.zipCode}</Text>
 
-                    <Forecast {...forecastInfo} />
+        <ImageBackground source={require("../bg1.jpg")} style={styles.backdrop}>
 
-
-                </View>
+            <View style={styles.highlight}>
 
 
-                
-                    
-                
-                
-                
-               
-                
-                
-                
-                
+                <Text style={styles.textStyle}>Zip Code is {props.zipCode}</Text>
 
-            </ImageBackground>
-            
-        
+                <Forecast {...forecastInfo} />
+
+
+            </View>
+
+
+        </ImageBackground>
+
+
     );
 }
 
@@ -56,7 +65,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         // paddingTop: Constants.statusBarHeight
-        
+
     },
 
     highlight: {
@@ -65,13 +74,13 @@ const styles = StyleSheet.create({
         height: '35%',
         alignItems: 'center',
         paddingTop: Constants.statusBarHeight
-        
-       
+
+
 
     },
 
     textStyle: {
-       fontSize: 20,
+        fontSize: 20,
         color: 'white',
         textAlign: 'center'
     }
